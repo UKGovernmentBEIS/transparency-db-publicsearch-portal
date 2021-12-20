c8d3c768f3f4b3d0e463c19897b467dbc5900088
James Holder <james.holder@cgi.com>
2021-12-20 14:46:22 +0000

    Add enpoint and route for schemes

--- app.js
+++ app.js
@@ -172,6 +172,9 @@ app.use("/legalgrantingdate", legalgrantingdate);
 var searchresults = require("./routes/searchresults");
 app.use("/searchresults", searchresults);
 
+var searchresults = require("./routes/schemes");
+app.use("/searchresults/schemes", searchresults);
+
 var pageroute = require("./routes/pageroute");
 app.use("/pageroute", pageroute);
 
