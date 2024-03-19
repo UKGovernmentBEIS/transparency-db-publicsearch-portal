34e1a95c6cc604490da9703d38c3cffad3fc80db
RyanHumphreys00 <ryan.humphreys@cgi.com>
2024-03-19 18:25:40 +0000

    feat: Added list of associated awards on scheme details page
    Shows a list of awards associated with the subsidy measure currently being viewed with pagination. Filtering supported but not used

--- routes/schemedetailsroute.js
+++ routes/schemedetailsroute.js
@@ -6,6 +6,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 var request = require("request");
+var utils = require("../utils");
 
 router.get("/", async (req, res) => {
   res.set("X-Frame-Options", "DENY");
@@ -18,28 +19,76 @@ router.get("/", async (req, res) => {
   scheme = req.query.scheme;
   console.log("scnumber : " + scheme);
   var measureendpoint =
-    beis_url_publicsearch + "/schemes/scheme/" + scheme;
+    beis_url_publicsearch + "/schemes/scheme/withawards/" + scheme;
 
+  currentPage = 1;
+  if (req.query.hasOwnProperty("page")) {
+    var pageParse = parseInt(req.query.page);
+    if (!isNaN(pageParse)) {
+      if(typeof totalPages === "undefined")
+        totalPages = 1;
+      currentPage = Math.max(1, Math.min(pageParse, totalPages));
+    }
+  }
+  prevPage = Math.max(1, currentPage - 1);
+  perPage = 10;
+  if (req.query.hasOwnProperty("perPage")) {
+    var perPageParse = parseInt(req.query.perPage);
+    if (!isNaN(perPageParse)) {
+      perPage = perPageParse;
+    }
+  }
   try {
-    const measureapidata =await axios.get(measureendpoint);
-    console.log(`Status: ${measureapidata.status}`);
-    console.log("Body: ", measureapidata.data);
-    searchmeasuredetails = measureapidata.data;
-    if(typeof searchmeasuredetails.spendingSectors !== 'undefined'){
-      var spendingSectorArray = JSON.parse(searchmeasuredetails.spendingSectors);
-    }
-    if(measureapidata.data.status == "Deleted"){
-      res.render("publicusersearch/noresults");
-    }else{
-      res.render("publicusersearch/schemedetails",{
-        spendingSectorArray,
-      });
-    }
+    const awardRequest = {
+      scNumber: scheme,
+      pageNumber: currentPage,
+      totalRecordsPerPage: perPage,
+      sortBy: ["awardNumber,desc"],
+    };
+    await axios.post(
+      measureendpoint,
+      awardRequest
+    ).then((response) => {
+      console.log(`Status: ${response.status}`);
+      console.log("Body: ", response.data);
+      searchmeasuredetails = response.data;
+      totalSearchResults = response.data.awardSearchResults.totalSearchResults;
+      totalPages = response.data.awardSearchResults.totalPages;
+      hasAwards = totalSearchResults > 0;
+      console.log(hasAwards);
+      console.log(totalSearchResults)
+      startRecord = ((currentPage - 1) * perPage) + 1;
+      endRecord = Math.min((currentPage * perPage), totalSearchResults);
+
+      searchawards = response.data.awardSearchResults.awards;
+      nextPage = Math.min(totalPages, currentPage + 1);
+      pagingStart = Math.max(1, currentPage - 5);
+      pagingEnd = Math.min(totalPages, currentPage + 5);
+
+      var spendingSectorArray = new Array();
+      if(typeof searchmeasuredetails.spendingSectors !== 'undefined'){
+        var spendingSectorArray = JSON.parse(searchmeasuredetails.spendingSectors);
+      }
+
+      if(response.data.status == "Deleted")
+      {
+        res.render("publicusersearch/noresults");
+      }
+      else
+      {
+        res.render("publicusersearch/schemedetails",
+        {
+          currentURI: req.protocol + '://' + req.get('host') + req.originalUrl,
+          spendingSectorArray,
+
+        });
+      }
+    });
   } catch (err) {
 
     if (err.toString().includes("404")) {
       res.render("publicusersearch/noresults");
-      console.warn("No results found for award number " + scheme);
+      console.warn("No results found for scheme number " + scheme);
     } else {
       console.error(err);
     }
