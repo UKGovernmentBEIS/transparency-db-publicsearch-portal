be8d305ea6ce0f87df875a2bd77df29d2d89f464
James Holder Barker <james.holderbarker@cgi.com>
2026-04-29 15:09:40 +0100

    move enviroment variable definitions to app.js

--- app.js
+++ app.js
@@ -133,6 +133,40 @@ app.locals.beis_url_publicsearch;
 app.locals.beis_url_accessmanagement;
 app.locals.awardnumber;
 
+
+// ********************************************************
+// Read environment property file and set the API URL end points
+// ********************************************************
+
+Environment_variable = process.argv[2];
+if (Environment_variable == "env=local") {  
+  beis_url_publicsearch =
+      "http://localhost:8581"; //http://localhost:8581
+
+  console.log(beis_url_publicsearch);
+} else if (Environment_variable == "env=dev") {
+  beis_url_publicsearch =
+      "https://dev-transparency-db-public-search-service.azurewebsites.net";
+
+  console.log(beis_url_publicsearch);
+} else if (Environment_variable == "env=integ") {
+  beis_url_publicsearch =
+    "https://integ-transparency-db-public-search-service.azurewebsites.net";
+  console.log(beis_url_publicsearch);
+} else if (Environment_variable == "env=stg") {
+  beis_url_publicsearch =
+    "https://stg-transparency-db-public-search-service.azurewebsites.net";
+  console.log(beis_url_publicsearch);
+} else if (Environment_variable == "env=prod") {
+  beis_url_publicsearch =
+    "https://prod-transparency-db-public-search-service.azurewebsites.net";
+  console.log(beis_url_publicsearch);
+} else if (Environment_variable == "env=preprod") {
+  beis_url_publicsearch =
+    "https://default-transparency-db-public-search-service.azurewebsites.net";
+  console.log(beis_url_publicsearch);
+}
+
 /***************************************************** */
 /* Default login screen - Web application Launch screen */
 /****************************************************** */
