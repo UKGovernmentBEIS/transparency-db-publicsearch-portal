e50698628b9e33fa2ca2ab28aaf24c63bdb734ea
Ananya Majumdar <a.majumdar@cgi.com>
2026-05-06 15:28:28 +0100

    added cross origin policy headers: resource (corp), opener (coop), and embedder (coep)

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ app.get("/", (req, res) => {
   res.set("Content-Security-Policy", 'frame-ancestors "self"');
   res.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
   res.set("Referrer-Policy", "origin");
-  res.set("Cross-Origin-Resource-Policy", "cross-origin");
+  res.set("Cross-Origin-Resource-Policy", "same-origin");
   res.set("Cross-Origin-Opener-Policy", "same-origin");
   res.set("Cross-Origin-Embedder-Policy", "require-corp");
   res.set("Access-Control-Allow-Origin", beis_url_publicsearch);
--- routes/submitfeedback.js
+++ routes/submitfeedback.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
   res.set("Access-Control-Allow-Origin", beis_url_publicsearch);
   res.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
   res.set("Referrer-Policy", "origin");
-  res.set("Cross-Origin-Resource-Policy", "cross-origin");
+  res.set("Cross-Origin-Resource-Policy", "same-origin");
   res.set("Cross-Origin-Opener-Policy", "same-origin");
   res.set("Cross-Origin-Embedder-Policy", "require-corp");
 
