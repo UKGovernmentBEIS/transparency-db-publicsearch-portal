8bfc134e4d4c14493a049286683fb920a6414309
James Holder Barker <james.holderbarker@cgi.com>
2026-06-22 14:04:41 +0100

    Allow export script to run and request from public search service

--- utils.js
+++ utils.js
@@ -27,11 +27,11 @@ exports.setSecurityHeaders = function (res, url) {
     "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
     "Content-Security-Policy": [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data:",
       "font-src 'self' data:",
-      "connect-src 'self'",
+      "connect-src 'self' " + url,
       "object-src 'none'",
       "base-uri 'self'",
       "form-action 'self'",
